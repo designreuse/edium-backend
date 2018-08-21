bcf7e52d1e4739da0fe73d940c1ffbf16800f7cd
Hồ Văn Hiền <hohien@Hos-MacBook-Pro.lan>
2018-08-21 10:52:45 +0700

    aws cli

---
+++ check_deploy.sh
@@ -0,0 +1,28 @@
+deploystart=$(date +%s)
+timeout=3000 # Seconds to wait before error. If it's taking awhile - your boxes probably are too small.
+threshhold=$((deploystart + timeout))
+while true; do
+    # Check for timeout
+    timenow=$(date +%s)
+    if [[ "$timenow" > "$threshhold" ]]; then
+        echo "Timeout - $timeout seconds elapsed"
+        exit 1
+    fi
+
+    # See what's deployed
+    current_version=`aws elasticbeanstalk describe-environments --application-name $APP_NAME --environment-name $ENV_NAME --query "Environments[*].VersionLabel" --output text`
+
+    status=`aws elasticbeanstalk describe-environments --application-name $APP_NAME --environment-name $ENV_NAME --query "Environments[*].Status" --output text`
+
+    if [ "$current_version" != "$VERSION" ]; then
+        echo "Tag not updated (currently $version). Waiting."
+        sleep 10
+        continue
+    fi
+    if [ "$status" != "Ready" ]; then
+        echo "System not Ready -it's $status. Waiting."
+        sleep 10
+        continue
+    fi
+    break
+done
